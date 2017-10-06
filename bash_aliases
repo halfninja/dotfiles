52379fbbf5b53cf7e00c84c907a42486c045bb58
Nick Howes <n.howes@warwick.ac.uk>
2017-10-06 11:02:03 +0100

    Today's the day

---
+++ bash_aliases
@@ -0,0 +1,4 @@
+alias gs="git status"
+alias gd="git diff"
+alias gdc="git diff --staged"
+alias gla="git log --all --graph"
--- puppet/update_home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR=`dirname $0`
-
-sudo puppet apply --modulepath "${DIR}/modules" "${DIR}/manifests/home.pp"
